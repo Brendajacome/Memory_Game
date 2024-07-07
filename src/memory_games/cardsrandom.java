4b7c568c6dfdc0ad5cc1fe09dc72a3f334bff01a
Brendajacome <bcjacomea@uce.edu.ec>
2024-07-07 14:14:04 -0500

    Creating Code and images in Easy Mode

---
+++ src/memory_games/cardsrandom.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package memory_games;
+
+import java.security.SecureRandom;
+
+/**
+ *
+ * @author Usuario
+ */
+public class cardsrandom extends SecureRandom {
+
+    public cardsrandom() {
+    }
+    
+}
